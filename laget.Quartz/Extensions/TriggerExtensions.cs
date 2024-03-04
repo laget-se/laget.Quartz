a26333623f9a636e9d181f9e64800f1b3cdba968
Erik Hennerfors <erik.hennerfors@laget.se>
2024-03-04 07:41:27 +0100

    Updated nuget(s)

--- laget.Quartz/Extensions/TriggerExtensions.cs
+++ laget.Quartz/Extensions/TriggerExtensions.cs
@@ -5,6 +5,6 @@ namespace laget.Quartz.Extensions
 {
     public static class TriggerExtensions
     {
-        public static string GetNextFireTime(this ITrigger trigger) => trigger.GetNextFireTimeUtc()?.DateTime.ToString(CultureInfo.CurrentCulture) ?? string.Empty;
+        public static string GetNextFireTime(this ITrigger trigger) => trigger.GetNextFireTimeUtc()?.LocalDateTime.ToString(CultureInfo.CurrentCulture) ?? string.Empty;
     }
 }
